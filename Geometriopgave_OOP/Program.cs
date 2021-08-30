f128c912fddb161c5639b27fc153c426bcb98db0
Jakob <Jakob@MSI>
2021-08-30 09:45:42 +0100

    Add project files.

---
+++ Geometriopgave_OOP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Geometriopgave_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Side_A kan herfra manipuleres.
+
+            Square Square1 = new Square();
+            
+
+        }
+    }
+}
---
+++ Geometriopgave_OOP/Square.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometriopgave_OOP
+{
+   //Denne class skal definere side A i vores firkant. 
+    public class Square
+    {
+        //Dette er den faste værdi af Side A
+        private int Side_A;
+
+        //Her kan værdien "Side A" manipuleres via. get og set
+        public int Side_a
+        {
+            get
+            {
+                return this.Side_A;
+            }
+            set
+            {
+                this.Side_A = value;
+            }
+        }
+        public int Perimeter
+        {
+            get
+            {
+                return this.Perimeter;
+            }
+            set
+            {
+                this.Perimeter = Side_A * 4;
+            }
+        }
+    }   
+}
